fc0eeb0e8dbd32c9396c662a286934d47ed36cfa
bru <brugera.herrero@gmail.com>
2025-01-19 18:01:27 +0100

    basic file structure

---
+++ src/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="Navbar">
+      <h1>Logo</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Welcome to the Home Page</h1>
+      <p>Start building your app here!</p>
+    </div>
+  );
+}
+
+export default Home;
