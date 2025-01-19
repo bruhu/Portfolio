0210b6f0bd2ec86299be5cddaa2cf38708d31af7
bru <brugera.herrero@gmail.com>
2025-01-19 19:09:25 +0100

    fixes for about section, add social buttons

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import './About.css'; // Ensure this line is present
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import './About.css';
 
 const About = () => {
   return (
@@ -11,8 +13,17 @@ const About = () => {
       <p className="about-me-text">After a break to focus on family, I'm now looking forward to bringing my analytical and development skills to new opportunities.</p>
       <p className="about-me-text">Outside of work, I've always been passionate about photography, which started as my profession and has since become a personal hobby, especially in the analog space. I'm also into bouldering, enjoy watching movies, and I'm an avid bookworm. Nature is a big part of my life, providing a source of inspiration and balance.</p>
       <p className="about-me-text">Let's connect!</p>
+    {/* Buttons for GitHub and LinkedIn */}
+    <div className="button-container">
+      <a href="https://github.com/bruhu" className="social-button github" target="_blank" rel="noopener noreferrer">
+        <GitHubIcon /> GitHub
+      </a>
+      <a href="https://linkedin.com/in/brugera" className="social-button linkedin" target="_blank" rel="noopener noreferrer">
+        <LinkedInIcon /> LinkedIn
+      </a>
     </div>
-  );
+  </div>
+);
 };
 
 export default About;
